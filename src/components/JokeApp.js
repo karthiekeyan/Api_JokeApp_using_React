41ed9b68daeb825bd9a680793953639650d5cc91
karthikeyan <karthikeyan05012@gmail.com>
2024-02-23 05:19:38 -0800

    change the code

--- src/components/JokeApp.js
+++ src/components/JokeApp.js
@@ -21,9 +21,11 @@ const JokeApp = () => {
     function Jokes(){
         alert("Go to click the Get Joke Button!");
     }
+   
 
   return (
-    <div className='bg-[#71717a] flex max-w-[1366px] h-[900px] justify-center text-center items-center p-7 md:[800px] border-spacing-0 backdrop-blur-2xl'>
+    
+    <body className='h-screen bg-[#71717a] flex  justify-center text-center items-center p-7 md:[800px] border-spacing-0 backdrop-blur-2xl'>
         <div className='bg-[#f1f5f9] p-8 w-[1000px]  rounded-[20px] '>
 
             <h1 className=' text-5xl pb-6 cursor-pointer text-black font-bold' onClick={Jokes}>JOKE APP<b className='hover:text-[#ff9c9c]'>!</b></h1>
@@ -43,7 +45,7 @@ const JokeApp = () => {
             <p className='font-medium mt-2'>You have read<b className='bg-black text-white px-3 py-1 rounded-full mx-2 hover:bg-[#ffcfcf] hover:text-[black]'>{Count}</b>Pieces of Joke!</p>
         </div> 
     </div>
-</div>
+</body>
   )
 }
 
